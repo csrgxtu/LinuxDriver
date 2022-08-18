adaf6d294db7eb0adbe1bad9f91609e14ec2167d
muyakongali <muyakongali@tencent.com>
2022-08-18 17:25:00 +0800

    make file use tab instead of spaces

--- HelloWorld/helloworld.c
+++ HelloWorld/helloworld.c
@@ -1,17 +1,19 @@
-#include <linux/init.h>
 #include <linux/module.h>
-MODULE_LISENCE("Dual BSD/GPL");
 
-static int hello_init(void)
+static int __init m_init(void)
 {
-    printk(KERN_ALERT "Hello, world\n");
+    printk(KERN_ALERT "Hello, world!\n");
     return 0;
 }
 
-static void hello_exit(void)
+static void __exit m_exit(void)
 {
-    printk(KERN_ALERT "Goodbye, cruel world\n");
+    printk(KERN_ALERT "Bye, world!\n");
 }
 
-moudle_init(hello_init);
-module_exit(hello_exit);
+module_init(m_init);
+module_exit(m_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Douglas Su");
+MODULE_DESCRIPTION("Hello World program");
